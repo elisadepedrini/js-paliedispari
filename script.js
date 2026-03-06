c9d27ed35557a88e2fc44c3d572a62aaf66cfb43
elisadepedrini <elisa.depedrini@gmail.com>
2026-03-06 17:36:47 +0100

    Complete task 1

--- script.js
+++ script.js
@@ -2,6 +2,32 @@
 Chiedere all’utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma */
 
+const oneWord = prompt('Enter a word', 'Ex. Cat')
+
+// Funzione per invertire la parola
+function reverseWord() {
+  return oneWord.split('').reverse().join('');
+};
+
+console.log(reverseWord());
+const wordReversed = reverseWord()
+console.log(wordReversed);
+
+
+// Funzione per verificare che la parola è palindroma 
+function palindrome() {
+    if (oneWord == wordReversed) {
+        console.log('Your word is palindrome');
+        
+        
+    } else {
+        console.log('Your word is not palindrome');
+    }
+}
+
+console.log(palindrome());
+
+
 
 /* 2. Pari e Dispari
 L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
